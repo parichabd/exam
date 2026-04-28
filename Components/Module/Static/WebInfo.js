20d3d6afddce4810c3c686183bd1884e445c7316
Parichehr <alfa@Caroline-Adkins.local>
2026-04-27 21:44:23 -0700

    fix image error 3

--- Components/Module/Static/WebInfo.js
+++ Components/Module/Static/WebInfo.js
@@ -9,23 +9,39 @@ function WebInfo() {
       <Divider/>
       <div className={styles.mainDiv}>
         <div className={styles.info}>
-          <Image src="/SVG/static/1.svg" alt="pic" width={80} height={70}/>
+          <Image 
+            src="/SVG/static/1.svg" 
+            alt="pic" 
+            width={80} 
+            height={70}
+            style={{ width: "auto", height: "auto" }}
+          />
           <div className={styles.eachParagraph}>
             <h2>بصرفه ترین قیمت</h2>
             <p>بصرفه ترین و ارزان ترین قیمت تور را از ما بخواهید.</p>
           </div>
         </div>
-
         <div className={styles.info}>
-          <Image src="/SVG/static/2.svg" alt="pic" width={80} height={70}/>
+          <Image 
+            src="/SVG/static/2.svg" 
+            alt="pic" 
+            width={80} 
+            height={70}
+            style={{ width: "auto", height: "auto" }}
+          />
           <div className={styles.eachParagraph}>
             <h2>پشتیبانی</h2>
             <p>پشتیبانی و همراهی 24 ساعته در تمامی مراحل سفر شما.</p>
           </div>
         </div>
-
         <div className={styles.info}>
-          <Image src="/SVG/static/3.svg" alt="pic" width={80} height={70}/>
+          <Image 
+            src="/SVG/static/3.svg" 
+            alt="pic" 
+            width={80} 
+            height={70}
+            style={{ width: "auto", height: "auto" }}
+          />
           <div className={styles.eachParagraph}>
             <h2>رضایت کاربران</h2>
             <p>رضایت بیش از 10هزار کاربر از تور های ما.</p>
